35e58b119cd3e069a6b8aef0340baaa40095987b
bwilukiroelisha <bwilukiroelisha@gmail.com>
2024-07-13 22:10:41 +0300

    Python Basic Operation

---
+++ 2_basics.py
@@ -0,0 +1,39 @@
+# Operations
+# a = 10
+# b = 20
+# c = 5
+
+# c = a + b
+# print('Addition \n a: {} b: {} c: {}'.format(a, b, c))
+# c = a - b
+# print('Subtraction \n a: {} b: {} c: {}'.format(a, b, c))
+# c = a * b
+# print('Multiplication \n a: {} b: {} c: {}'.format(a, b, c))
+# c = a / b
+# print('Division \n a: {} b: {} c: {}'.format(a, b, c))
+
+
+# Mathematic
+# width = int(input('Enter the width '))
+# height = int(input('Enter the height '))
+# length = int(input('Enter the length '))
+# area = width * height
+# volume = width * height * length
+# print('\nThe area of the rectangle is ', area, 'square units')
+# print('The volume of the rectangle is ', volume, 'cubic square units')
+
+
+# Getting user input
+# input_one = input('Please enter your input ')
+# print('You input is ', input_one)
+
+
+# Exercise
+# user_name = input('What is your name? ')
+# print('Hello ',user_name,' have a nice day')
+
+# Pythagoras Theorem
+side_a = int(input('Enter the width of the rectangle : '))
+side_b = int(input('Enter the height of the rectangle : '))
+hypotenuse = ((side_a ** 2) + (side_b ** 2)) ** (0.5)
+print('The length of the hypotenuse is : ', hypotenuse)
