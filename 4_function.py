faf1deeca53beb086bdcaf53a5ed381b6ecb4da6
bwilukiroelisha <bwilukiroelisha@gmail.com>
2024-08-01 02:20:37 +0300

    Function added

---
+++ 4_function.py
@@ -0,0 +1,4 @@
+def square(number):
+    return number ** 2
+object = square(6)
+print('Result is ',object)
---
+++ 6_keywords.py
@@ -0,0 +1,7 @@
+
+# Importing Keyword Library
+# Print all built-in keywords in Python
+
+import keyword
+print('\n\tBuilt-in keywords in Python\n')
+print(keyword.kwlist)
