5c25a3f8e6305dc1805712942c9d57343e4f5b10
bwilukiroelisha <bwilukiroelisha@gmail.com>
2024-07-31 11:49:30 +0300

    If condition

--- 3_control_statement.py
+++ 3_control_statement.py
@@ -1,5 +1,7 @@
 
-# If statements
+# IF STATEMENT
+# Exercise 1: Compare the current value entered
+"""
 value = int(input('Enter the value : ')) 
 output = ''
 if value > 100 :
@@ -8,4 +10,35 @@ elif value < 100 :
     output = 'Value : ' + str(value) + ' is less than 100'
 else:
     output = 'Value : ' + str(value) + ' is equal to 100'
+
 print(output)
+"""
+
+# Exercise 2: A program to check if a person is eligible for voting or not
+"""
+age = int(input('Enter the age: '))
+if age >= 18:
+    output = 'Age ' + str(age) + ', a person is eligible for voting'
+else:
+    output = 'Age ' + str(age) + ', a person is not eligible for voting'
+
+print(output)
+"""
+
+# Exercise 3: A program to check a marks of a student
+"""
+marks = int(input('Enter the marks: '))
+if marks > 80 and marks <=100 :
+    output = 'Marks : ' +str(marks)+ ', Congratulation! You scored grade A'
+elif marks > 60 and marks <= 80 :
+    output = 'Marks : ' + str(marks)+ ', You scored grade B+'
+elif marks > 40 and marks <= 60 :
+    output = 'Marks : ' + str(marks)+ ', You scored grade B'
+elif marks > 30 and marks <= 40 :
+    output = 'Marks : ' + str(marks)+ ', You scored grade C'
+else :
+    output = 'Marks : ' + str(marks)+ ', You scored grade F'
+
+print(output)
+"""
+
